6d7a9ebd68d42dcf2e851137449df7a5042a9028
Gisterox123 <83808233+Gisterox123@users.noreply.github.com>
2022-09-13 17:41:06 +0800

    Create 14channel.go

---
+++ com.ppku/14channel.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	/*
+			通道的声明
+			var chanName chan channelType
+		 	var ch chan string
+			ch = make(chan string)
+			ch:=make(chan string)
+
+			发送数据
+			channel <-data
+			接受数据
+			value:= <-channel
+	*/
+
+}
