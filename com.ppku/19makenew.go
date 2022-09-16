24c7431c2815b2b8fdbcac9024d23173e096e640
Gisterox123 <83808233+Gisterox123@users.noreply.github.com>
2022-09-16 17:32:56 +0800

    Update 19makenew.go

--- com.ppku/19makenew.go
+++ com.ppku/19makenew.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	/*
-		new 为所有类型分配内存，并且初始化零值，返回指针
-		make 只能为slice,map,chan 分配内存，并且初始化，返回的是类型
+			new 为所有类型分配内存，并且初始化零值，返回指针
+			make 只能为slice,map,chan 分配内存，并且初始化，返回的是类型
+		1. make 分配空间后，会进⾏初始化，new分配的空间被清零
+		2. new 分配返回的是指针，即类型 *Type。make 返回引⽤，即 Type；
+		3. new 可以分配任意类型的数据
 	*/
 
 	num := new(int)
